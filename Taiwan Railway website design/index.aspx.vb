f0a2ea80a8c977ae38649673a92acb05bbbc74d3
dog14230pp <43878307+dog14230pp@users.noreply.github.com>
2019-10-02 17:03:12 +0800

    Create index.aspx.vb

---
+++ Taiwan Railway website design/index.aspx.vb
@@ -0,0 +1,11 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("Booking.aspx")
+    End Sub
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Response.Redirect("Search_time.aspx")
+    End Sub
+End Class
