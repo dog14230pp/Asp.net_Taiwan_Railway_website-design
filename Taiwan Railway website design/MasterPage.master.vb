e6847c7c3e7e73a45ddbe7afeb1cd19b0efb0d7c
dog14230pp <43878307+dog14230pp@users.noreply.github.com>
2019-10-02 17:03:10 +0800

    Create MasterPage.master.vb

---
+++ Taiwan Railway website design/MasterPage.master.vb
@@ -0,0 +1,14 @@
+﻿
+Partial Class MasterPage
+    Inherits System.Web.UI.MasterPage
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("Booking.aspx")
+    End Sub
+    Protected Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Response.Redirect("Search_Booking.aspx")
+    End Sub
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Response.Redirect("Search_time.aspx")
+    End Sub
+End Class
+
