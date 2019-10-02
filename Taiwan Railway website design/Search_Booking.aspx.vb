339ff2f2f93a0993c34d597756c8656a295439fd
dog14230pp <43878307+dog14230pp@users.noreply.github.com>
2019-10-02 17:03:29 +0800

    Create Search_Booking.aspx.vb

---
+++ Taiwan Railway website design/Search_Booking.aspx.vb
@@ -0,0 +1,10 @@
+﻿
+Partial Class Search_Booking
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GridView1.ShowHeaderWhenEmpty = True
+        GridView1.EmptyDataText = "查無資料!!!"
+    End Sub
+
+End Class
